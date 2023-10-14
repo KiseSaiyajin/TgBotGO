d41d5e9c81b9176a4b4bfa4aba7986fc88163f57
Rafael <trafsel2000gmail.com>
2023-10-14 20:11:58 +0600

    Add simple commands!

--- main.go
+++ main.go
@@ -23,12 +23,32 @@ func main() {
 
 	for update := range updates {
 		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			if update.Message.IsCommand() {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+				switch update.Message.Command() {
+				case "help":
+					msg.Text = "type /sayhi or /status."
+				case "sayhi":
+					msg.Text = "Hi :)"
+				case "status":
+					msg.Text = "I'm ok."
+				case "withArgument":
+					msg.Text = "You supplied the following argument: " + update.Message.CommandArguments()
+				case "html":
+					msg.ParseMode = "html"
+					msg.Text = "This will be interpreted as HTML, click <a href=\"https://www.example.com\">here</a>"
+				default:
+					msg.Text = "I don't know that command"
+				}
+				bot.Send(msg)
+			} else {
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+				msg.ReplyToMessageID = update.Message.MessageID
 
-			bot.Send(msg)
+				bot.Send(msg)
+			}
 		}
 	}
 }
